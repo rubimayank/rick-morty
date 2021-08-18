27fdefd9ca3d3ebe9053a460a3dc76f5dc044ed6
rubimayank <rubita.kumari@gmail.com>
2021-08-18 22:45:39 +0530

    docs: adds attribute props docs

--- src/components/Attributes/index.tsx
+++ src/components/Attributes/index.tsx
@@ -4,13 +4,21 @@ import { Tag } from 'antd';
 import styles from './styles.module.css';
 import { FilterAction } from '../../types';
 
-export default function Attributes({
-  tags = {},
-  onFilter,
-}: {
+/**
+ * Attributes component props
+ */
+export interface AttributeProps {
+  /**
+   * an object of attributes with attribute's id as key and attributes's value as value
+   */
   tags?: Record<string, string | null | undefined>;
+  /**
+   * onclick callback for applying filters when used in list cards
+   */
   onFilter?: FilterAction;
-}): ReactElement {
+}
+
+export default function Attributes({ tags = {}, onFilter }: AttributeProps): ReactElement {
   return (
     <div className={styles.tags} data-clickable={!!onFilter || undefined}>
       {Object.entries(tags).map(([key, val]) => (
